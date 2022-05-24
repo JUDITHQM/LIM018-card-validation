a7fe5accf960f986fdbc8ca3016938dcf677f253
Judith Quispe <jhqm.98@gmail.com>
2022-05-24 00:01:00 -0500

    Implemento del codigo para capturar elementos del DOM y ejecutar la funcion validacion

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-import validator from './validator.js';
-
-console.log(validator);
+import validator from "./validator.js";
+const formElement=document.querySelector("form")//Almacenar el nodo form
+function onSubmitForm() { //Funcion de evaluacion cuando suceda el evento submit
+    formElement.addEventListener("submit",(event)=>{
+        const formData=new FormData(formElement)
+        const numeroTarjeta=formData.get("numerotarjeta")
+        if (validator.isValid(numeroTarjeta)) {
+            alert("Tarjeta Valida")
+        }
+        else {
+            alert("Tarjeta No valida")
+        }
+    })
+}
+onSubmitForm() //ejecutar
