e4617d7140b43d38365b1c33404d4d271146b6ab
Judith Quispe <jhqm.98@gmail.com>
2022-07-19 12:09:21 -0500

    Añadiendo estilos

--- src/index.js
+++ src/index.js
@@ -1,15 +1,40 @@
-import validator from "./validator.js";
-const formElement=document.querySelector("form")//Almacenar el nodo form
+import validator from "./validator.js"; // importar el modulo validator.js
+const formElement=document.querySelector("form")//Captura el contenido de la etiqueta form del html (nodo)
 function onSubmitForm() { //Funcion de evaluacion cuando suceda el evento submit
     formElement.addEventListener("submit",(event)=>{
-        const formData=new FormData(formElement)
-        const numeroTarjeta=formData.get("numerotarjeta")
-        if (validator.isValid(numeroTarjeta)) {
-            alert("Tarjeta Valida")
-        }
-        else {
-            alert("Tarjeta No valida")
-        }
+      event.preventDefault() //Permite que nodo capturado ,escuche el evento Submit y ejecute una operacion
+        const formData=new FormData(formElement) // El nodo capturado lo convierte en objeto
+        const numeroTarjeta=formData.get("numerotarjeta") // Accede al dato ingresado del campo de tarjeta
+        if (validator.isValid(numeroTarjeta)) { //Llamar a la funcion validation
+
+          const fieldNumberPassword=document.getElementById('inumerotarjeta')
+
+          const hidenNumberTarget=validator.maskify(numeroTarjeta)
+          fieldNumberPassword.value=hidenNumberTarget
+          const messageElementSucces=document.querySelector('.message-validation-success');
+          messageElementSucces.style.display="block"
+          messageElementSucces.innerHTML=`Tarjeta Valida`
+          const messageElementError=document.querySelector('.message-validation-error');
+          messageElementError.style.display="none"
+      }
+      else {
+        const fieldNumberPassword=document.getElementById('inumerotarjeta')
+        const hidenNumberTarget=validator.maskify(numeroTarjeta)
+        fieldNumberPassword.value=hidenNumberTarget
+        const messageElement=document.querySelector('.message-validation-error');
+        messageElement.style.display="block"
+        messageElement.innerHTML=`Tarjeta Invalida`
+        const messageElementSuccess=document.querySelector('.message-validation-success');
+        messageElementSuccess.style.display="none"
+      }
     })
 }
+function cleanForm() {
+  const buttonReset=formElement.querySelector('.button-reset')
+  buttonReset.addEventListener('click',()=>{
+    window.location.href='index.html'
+  })
+
+}
 onSubmitForm() //ejecutar
+cleanForm()
--- src/validator.js
+++ src/validator.js
@@ -4,41 +4,53 @@ const validator = {
     .split('') //Para convertir una cadena string a un array
     .reverse() //Para invertir el orden de los numeros
     .map(x => parseInt(x)); //Para recorrer cada elemento del array y convertirlo a entero
-let doublePositionPair=arr.map((value,idx)=>{ //Declara una variable para doblar las posiciones pares y sumar los digitos mayores a 9
-  if ((idx+1)%2==0) {
-      value=value*2
-      if (value>9) {
+    console.log(arr);
+//Recorrer el arrays de digitos y doblar si es mayor de 9
+    let doublePositionPair=arr.map((value,idx)=>{ //Declara una variable para doblar las posiciones pares y sumar los digitos mayores a 9
+  if ((idx+1)%2==0) { //Evaluo si la posicion es par
+      value=value*2   // Si es para , que lo multiplique por dos
+      if (value>9) {  // Si es mayor que 9
           let converValuetoArray = (value + '') //Para convertir  un numero entero en cadena
           .split('') //Para convertir una cadena string a un array
           .map(x => parseInt(x)); //Para recorrer cada elemento del array y convertirlo a entero
-    
-          const initialValue = 0;
+          //Recorrer el array para sumar sus elementos
+          const initialValue = 0; //Valor inicial de la suma total
           const sumarDigitosArray = converValuetoArray.reduce( //Obtener un valor previo y un valor actual
               (previousValue, currentValue) => previousValue + currentValue,//Recorrer el array e ir sumando los valores
               initialValue // Parametro de inicio de la Suma
             );
                   return sumarDigitosArray
                 }else {
-                    return value
+                    return value //Si no es mayor a 9 , mantener el valor multiplicado por 2
                 }
-            
+
                 } else {
-                    return value+0
+                    return value+0 // Si no es una posicion par , entonces retorna el valor original
                 }
             })
+//Sumar los elementos del arrays
 const initialValue = 0; //Declarar un acumulador
-let sumAllDigits=doublePositionPair.reduce( //Definir una variable con funcion
-  (previousValue, currentValue) => previousValue + currentValue,
-  initialValue 
-); // Retorna la suma total y lo almacena en sumAllDigits
-if (sumAllDigits%10==0){ 
+let sumAllDigits=doublePositionPair.reduce( //Definir una variable con funcion sumAllDigits
+  (previousValue, currentValue) => previousValue + currentValue, // Iterando el array y sumando los valores
+  initialValue ); // Retorna la suma total y lo almacena en sumAllDigits
+if (sumAllDigits%10==0){  //Si la suma total es multiplo de 10 , con residuo 0
    return true
 }
 else {
     return false
 }
+ },
+maskify:(creditCardNumber)=>{
+    const size = creditCardNumber.length - 5;  //total de caracteres a enmascarar
+    let maskString = "";
+    for (let i=0; i < creditCardNumber.length; i++) {
+      if (i <= size){      //iterar deacuerdo a los índices menores al "total de caracteres a encriptar"
+        maskString = maskString + "#";   //sustituir por # y agregar a la variable en cada iteración
+      } else {
+        maskString = maskString + creditCardNumber[i]; //si no cumple agregar el número o letra
+      }
+    }
+   return maskString;
  }
-};
-
-export default validator;
-
+ };
+export default validator; // Exporta el objeto validator que tiene un metodo de validacion validator y un metodo de enmascarar caracteres
